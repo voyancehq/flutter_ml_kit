519cb2fbbcfdc458c42cba230a8795c664b457bb
Segun Sodimu <princesegzy01@yahoo.com>
2021-04-20 15:50:34 +0100

    refactor

--- example/android/app/src/main/java/com/voyance/voyance_ml_kit_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.voyance.voyance_ml_kit_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- ios/Classes/VoyanceMlKitPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface VoyanceMlKitPlugin : NSObject<FlutterPlugin>
-@end
