7eb41d3b4ca8194111664139b0ed750783623c94
Segun Sodimu <princesegzy01@yahoo.com>
2021-04-21 10:27:24 +0100

    refactor codebase

--- lib/capture/capture_home.dart
+++ lib/capture/capture_home.dart
@@ -2,29 +2,9 @@ import 'dart:io';
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_crop/image_crop.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:voyance_ml_kit/capture/capture_send.dart';
-// import 'package:voyanceapp/Page2.dart';}
-
-// import 'camera.dart';
-
-// void main() {
-//   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-//     statusBarColor: Colors.transparent,
-//     statusBarBrightness: Brightness.dark,
-//     statusBarIconBrightness: Brightness.light,
-//     systemNavigationBarIconBrightness: Brightness.light,
-//   ));
-//
-//   // runApp(new MyApp());
-//   runApp(MaterialApp(
-//     title: "App",
-//     debugShowCheckedModeBanner: false,
-//     home: MyApp(),
-//   ));
-// }
 
 class VCaptureHome extends StatefulWidget {
 
@@ -66,19 +46,7 @@ class _VCaptureHome extends State<VCaptureHome> {
         ),
       ),
     );
-    // );
 
-
-
-    // return SafeArea(
-    //     child: Container(
-    //       color: Colors.black,
-    //       padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
-    //       child: _sample == null ? _buildOpeningImage() : _buildCroppingImage(),
-    //     ),
-    //   );
-
-    // );
   }
 
   Widget _buildOpeningImage() {
@@ -116,37 +84,16 @@ class _VCaptureHome extends State<VCaptureHome> {
   }
 
   Widget _buildOpenImage() {
-    // return FlatButton(
-    //   child: Text(
-    //     'Open Image',
-    //     style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
-    //   ),
-    //   onPressed: () => _openImage(),
-    // );
+
     return Column(
       children: [
 
-        //   Container(
-        //       height: 200,
-        //       // width: 300,
-        //       alignment: Alignment.topLeft,
-        //       decoration: BoxDecoration(
-        //           color: Colors.white,
-        //       ),
-        //     child: _lastCropped != null ? Image.file(_lastCropped) : SizedBox(),
-        //   ),
-        // SizedBox(height: 10,),
         FlatButton(
           child: Text(
             'Camera',
             style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
           ),
-          // onPressed: () => _openImage(),
-          //   onPressed: () {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => Camera()),
-          // );
+
           onPressed: () => _openImage(1),
 
         ),
@@ -180,13 +127,9 @@ class _VCaptureHome extends State<VCaptureHome> {
   Future<void> _cropImage() async {
     final scale = cropKey.currentState.scale;
     final area = cropKey.currentState.area;
-    print(">>>>>>>>>>>>>>>>>.");
-    print(area.width);
-    print(area.height);
-    print("<<<<<<<<<<<<<<<<<<<<<");
+
     if (area == null) {
       // cannot crop, widget is not setup
-      print(">>>>>>>>>>>>>>>>>>>>>>>>>>> ");
       return;
     }
 
@@ -210,15 +153,6 @@ class _VCaptureHome extends State<VCaptureHome> {
     _lastCropped = file;
 
     debugPrint('$file');
-
-    // setState(() {
-    //   _sample = null;
-    // });
-
-
-    print('$_file');
-    print(" >>> " +_lastCropped.path);
-
 
     Navigator.push(
       context,
--- lib/capture/capture_send.dart
+++ lib/capture/capture_send.dart
@@ -23,68 +23,10 @@ class VCaptureSend extends StatefulWidget {
 
 class _VCaptureSend extends State<VCaptureSend> {
 
-
-  File _image;
-  File _imageFile;
-
-  double _imageWidth;
-  double _imageHeight;
-  bool _busy = false;
-
-  List _classifiedResult;
-
-  String _result = "";
-
-
   @override
   void initState() {
     super.initState();
-    // loadImageModel();
   }
-
-
-  // Future loadImageModel() async {
-  //   Tflite.close();
-  //   String result;
-  //   result = await Tflite.loadModel(
-  //     model: "assets/converted_tflite/model_unquant.tflite",
-  //     labels: "assets/converted_tflite/labels.txt",
-  //   );
-  //   print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-  //   print(result);
-  // }
-
-
-
-  // Future<String> classifyImage(image) async {
-  //   _classifiedResult = null;
-  //   // Run tensorflowlite image classification model on the image
-  //   print("classification start $image");
-  //   final List result = await Tflite.runModelOnImage(
-  //     path: image.path,
-  //     numResults: 6,
-  //     threshold: 0.05,
-  //     imageMean: 127.5,
-  //     imageStd: 127.5,
-  //   );
-  //   print("classification done");
-  //   setState(() {
-  //     if (image != null) {
-  //       _imageFile = File(image.path);
-  //       _classifiedResult = result;
-  //
-  //       String x = "Confidence : " + result[0]["confidence"].toString() + ", label : " + result[0]["label"].toString();
-  //       _result = x;
-  //
-  //       print(x);
-  //       return x;
-  //     } else {
-  //       print('No image selected.');
-  //
-  //     }
-  //   });
-  // }
-
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +34,7 @@ class _VCaptureSend extends State<VCaptureSend> {
       // title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Voyance Data Collection'),
         ),
         body: Center(
             child: Column(
@@ -118,7 +60,6 @@ class _VCaptureSend extends State<VCaptureSend> {
                     child: Text("Upload Image")
                 ),
                 SizedBox(height: 10,),
-                Padding(padding: EdgeInsets.all(10), child: Text("Result : "  + _result),)
               ],
             )
         ),
