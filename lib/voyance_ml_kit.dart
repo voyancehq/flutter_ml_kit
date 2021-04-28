2ca181bc479ebe0a1855c3cd9e729c8a06f9e726
Segun Sodimu <princesegzy01@yahoo.com>
2021-04-28 15:58:29 +0100

    add multiple methods api

--- lib/voyance_ml_kit.dart
+++ lib/voyance_ml_kit.dart
@@ -19,4 +19,57 @@ class MLKit {
       MaterialPageRoute(builder: (context) => VCaptureHome(Country: country, DocumentType: documentType, SecretKey: secretKey, AccessKey: accessKey, ResponseRoute: responseRoute)),
     );
   }
+
+  // isImageBlurry takes in an Image file as a parameter
+  // and check if the image is blurry by outputting boolean
+  bool isImageBlurry(Image image){
+    return false;
+  }
+
+  // getBlurry takes in an Image file as input and
+  // returns a double value of degree of bluriness
+  double getBlurry(Image image){
+    return 0.2;
+  }
+
+  // classifyDocument takes in an image as input
+  // then output the type of document in string
+  String classifyDocument(Image image){
+      return "";
+  }
+
+  // takes in an image file and document type as input
+  // return a boolean to detect if image is tampered with or not
+  bool isTampered(Image image, String documentType){
+    return true;
+  }
+
+  // takes in an image file as an input
+  // return a boolean to check if MRZ is available in the document
+  bool mrzChech(Image image){
+    return false;
+  }
+
+
+  // takes in an image file as an input
+  // return a map containing information of the MRZ
+  Map extractMrz(Image image){
+    return {};
+  }
+
+
+  // takes in an image file as an input
+  // return a boolean to check if Barcode is available in the document
+  bool qrCheck(Image image){
+    return false;
+  }
+
+  // takes in an image file as an input
+  // return a string containing information of the code
+  String extractQR(Image image){
+    return "";
+  }
+
+
+
 }
